6788d2b3c7f28b2f6c1bc645ea6fb90329f7b6bb
Zeamanuel-Admasu <zeamanshibru2020@gmail.com>
2025-06-30 11:50:21 +0300

    aa

--- oop/library_system.py
+++ oop/library_system.py
@@ -19,8 +19,8 @@ class PrintBook(Book):
 class Library:
     def __init__(self):
         self.books = []
-    def add_book(self):
-        self.books.append()
+    def add_book(self, book):
+        self.books.append(book)
     def list_books(self):
         for book in self.books:
             print(book)
